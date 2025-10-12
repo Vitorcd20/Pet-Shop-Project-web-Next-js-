714f0385a6d5865544486c77759cd2748bed43ae
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-12 18:35:33 -0300

    Refactor layout and page components for improved structure and updated metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-accent-blue">Hello</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:mb-8">
+        <div>
+          <h1 className="text-title text-content-primary mb-8">
+            Your schedule
+          </h1>
+          <p className="text-paragraph-small text-content-secondary">
+            Here you can see all your clients and the appointments scheduled for
+            today
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
