6c6a51f0fd766bad5cd92a359eaf902c56d659fe
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-13 18:02:08 -0300

    feat: creating input calendar

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,5 +1,12 @@
 "use client";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format, startOfDay, startOfToday } from "date-fns";
+import { CalendarIcon, ChevronDownIcon, Dog, Phone, User } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { IMaskInput } from "react-imask";
 import { z } from "zod";
+import { Button } from "../ui/button";
 import {
   Dialog,
   DialogContent,
@@ -8,9 +15,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
-import { Button } from "../ui/button";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -19,16 +23,23 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { Dog, Phone, User } from "lucide-react";
 import { Input } from "../ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Textarea } from "../ui/textarea";
-import { IMaskInput } from "react-imask";
+import { Calendar } from "../ui/calendar";
 
 const appointmentFormSchema = z.object({
   tutorName: z.string().min(3, "Tutor name is required"),
   petName: z.string().min(3, "Pet name is required"),
   phone: z.string().min(11, "Phone number is required"),
   description: z.string().min(3, "Description is required"),
+  scheduleAt: z
+    .date({
+      error: "Date is required",
+    })
+    .min(new Date(), {
+      message: "The date cannot be in the past",
+    }),
 });
 
 type AppointmentFormValue = z.infer<typeof appointmentFormSchema>;
@@ -41,6 +52,7 @@ export const AppointmentForm = () => {
       petName: "",
       phone: "",
       description: "",
+      scheduleAt: undefined,
     },
   });
 
@@ -161,6 +173,53 @@ export const AppointmentForm = () => {
                       {...field}
                     />
                   </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="scheduleAt"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Date
+                  </FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          className={cn(
+                            "w-full justify-between text-left font-normal bg-background-tertiary border-border-primary text-content-primary hover:bg-background-tertiary hover:border-border-secondary hover:text-content-primary focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-border-brand focus:border-border-brand focus-visible:border-border-brand",
+                            !field.value && "text-content-secondary"
+                          )}
+                        >
+                          <div className="flex items-center gap-2">
+                            <CalendarIcon
+                              className=" text-content-brand"
+                              size={20}
+                            />
+                            {field.value ? (
+                              format(field.value, "dd/MM/yyyy")
+                            ) : (
+                              <span>Select a date</span>
+                            )}
+                          </div>
+                          <ChevronDownIcon className="opacity-50 h-4 w-4" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date < startOfToday()}
+                      />
+                    </PopoverContent>
+                  </Popover>
                   <FormMessage />
                 </FormItem>
               )}
