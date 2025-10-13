eee869082189c6df402a68bac6cf1d225b683b5a
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-13 10:07:56 -0300

    Refactor appointment data management by moving mock data to a separate file and updating the Home component to use it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,8 @@
 import { PeriodSection } from "@/components/period-section/period-section";
-import { groupAppointmentByPeriod } from "@/utils/appointment-utils";
-
-const appointments = [
-  {
-    id: "1",
-    petName: "Rex",
-    description: "Consulta",
-    tutorName: "João",
-    phone: "1234567890",
-    scheduleAt: new Date("2025-08-17T10:00:00"),
-  },
-  {
-    id: "2",
-    petName: "Mimi",
-    tutorName: "Maria",
-    description: "Banho",
-    phone: "1234567890",
-    scheduleAt: new Date("2025-08-17T11:00:00"),
-  },
-  {
-    id: "3",
-    petName: "Nina",
-    tutorName: "Natalia",
-    description: "Consulta",
-    phone: "1234567890",
-    scheduleAt: new Date("2025-08-17T14:00:00"),
-  },
-  {
-    id: "4",
-    petName: "Nina",
-    tutorName: "Natalia",
-    description: "Consulta",
-    phone: "1234567890",
-    scheduleAt: new Date("2025-08-17T19:00:00"),
-  },
-];
-
+import { groupAppointmentByPeriod, APPOINTMENTS } from "@/utils";
 
 export default function Home() {
-  const periods = groupAppointmentByPeriod(appointments);
+  const periods = groupAppointmentByPeriod(APPOINTMENTS);
 
   return (
     <div className="bg-background-primary p-6">
