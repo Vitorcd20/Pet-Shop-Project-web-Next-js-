64427648bff10c61bb885740ad6421a10dfb5974
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-18 15:09:23 -0300

    feat: creating date-picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AppointmentForm } from "@/components/appointment-form/appointment-form";
+import { DatePicker } from "@/components/date-picker";
 import { PeriodSection } from "@/components/period-section/period-section";
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
@@ -36,6 +37,14 @@ export default async function Home({
             today
           </p>
         </div>
+
+        <div className="hidden md:flex items-center gap-4">
+          <DatePicker />
+        </div>
+      </div>
+
+      <div className="mt-3 mb-8 md:hidden">
+        <DatePicker />
       </div>
 
       <div className="pb-24 md:pb-0">
